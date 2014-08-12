1460e117b31616311ea1ea9d993e0c893493094a
Ron Forbes <ronforbes528@gmail.com>
2014-08-12 09:59:53 -0700

    A bit more code clean-up
    Removing unneeded commets

--- BlockPartyClient/Assets/Scripts/ParticleEffect.cs
+++ BlockPartyClient/Assets/Scripts/ParticleEffect.cs
@@ -5,13 +5,11 @@ public class ParticleEffect : MonoBehaviour
 {
     public int X, Y;
 
-    // Use this for initialization
     void Start()
     {
 	
     }
-	
-    // Update is called once per frame
+
     void Update()
     {
 	
